c73d4c2e5496ba773fe6314c914558621d69b912
罗世杰 <bloodclaster@luoshijiedeMacBook-Pro.local>
2022-04-07 16:33:32 +0800

    注册更新登陆接口初始化

--- src/services/login.tsx
+++ src/services/login.tsx
@@ -1,1 +1,37 @@
 import request from '@/utils/request';
+
+export async function register(data: {
+  email: string,
+  head: number,
+  id: number,
+  manager: number,
+  nickname: string,
+  password: string,
+}) {
+  return request('/user/register', {
+    method: 'POST',
+    data
+  });
+}
+
+export async function login(data: {
+  email: string,
+  password: string,
+}) {
+  return request('/user/login', {
+    method: 'POST',
+    data
+  });
+}
+
+export async function update(data: {
+  email: string,
+  head: number,
+  nickname: string,
+  password: string,
+}) {
+  return request('/user/update', {
+    method: 'POST',
+    data
+  });
+}
